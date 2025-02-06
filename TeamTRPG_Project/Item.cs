7d18583f9b601d210aa58bf49f3d9f5cc326832e
park8306 <kosaf0804@gmail.com>
2025-02-06 17:19:45 +0900

    [feat] Create Item Class

---
+++ TeamTRPG_Project/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTRPG_Project
+{
+    public enum ItemType
+    {
+        ATK,
+        DEF,
+        COUNT
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public ItemType ItemType { get; set; }
+        public float ATK { get; set; }
+        public float DEF { get; set; }
+        public bool IsEquip {  get; set; }
+
+        public Item(string name, ItemType itemType,float atk, float def)
+        {
+            Name = name;
+            ItemType = itemType;
+            ATK = atk;
+            DEF = def;
+            IsEquip = false;
+        }
+    }
+}
