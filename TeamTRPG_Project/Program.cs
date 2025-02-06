505a7d19c8e3bfb0a97d4c69f803a62df6eedf2c
Hwanyi <wksgo123@knu.ac.kr>
2025-02-06 17:52:58 +0900

    [feat] Create Character Class

--- TeamTRPG_Project/Program.cs
+++ TeamTRPG_Project/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Character ch = new Character("test");
+            ch.ShowInfo();
         }
     }
 }
