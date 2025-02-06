09d93babea1cb21934a19dd5f29a88dbbaa3b564
Gaon99 <shk9680@icloud.com>
2025-02-06 16:49:58 +0900

    Init Solution

---
+++ TeamTRPG_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamTRPG_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
