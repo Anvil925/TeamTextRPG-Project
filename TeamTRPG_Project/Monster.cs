bba748969f6cb903a2746e72e8da90a4095d1b1a
Anvil925 <anvil_925@naver.com>
2025-02-07 19:01:32 +0900

    [Feat] Monster 스트링 수정

--- TeamTRPG_Project/Monster.cs
+++ TeamTRPG_Project/Monster.cs
@@ -1,5 +1,5 @@
-using System;
-using TeamTRPG_Project;
+using System; 
+using TeamTRPG_Project; 
 public class Monster 
 {
     public string Name { get; private set; } // 몬스터 이름
@@ -36,7 +36,7 @@ public class Monster
 
     public override string ToString() // 문자열 반환 메서드 재정의
     {
-        return $"[Lv.{LV}] {Name} - HP: {HP}, ATK: {ATK}, DEF: {DEF}"; // 몬스터 정보 반환
+        return $"[경력:{LV}연차] {Name} - 멘탈: {HP}, 정치력: {ATK}, 아부력: {DEF}"; // 몬스터 정보 반환
     }
 
     public void AttackPlayer(Character player) // 플레이어를 공격하는 메서드
