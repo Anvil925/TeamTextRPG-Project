df05b913c72c69acade81c05c64f2ad5da330460
YoungJoong-kim98 <rladudwnd012@gmail.com>
2025-02-07 09:47:18 +0900

    [Docs] 주석 수정

--- TeamTRPG_Project/Program.cs
+++ TeamTRPG_Project/Program.cs
@@ -17,17 +17,16 @@
         }
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-=======
+
             StartStory();
             string Name;
->>>>>>> Stashed changes
+
             while (true)
             {
                 Console.Clear();
                 Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n원하시는 이름을 입력해주세요.");
                 Console.Write("=>");
-                string Name = Console.ReadLine();
+                Name = Console.ReadLine();
                 Console.WriteLine($"입력하신 이름은 {Name}입니다.\n1.저장 \n2.아니오");
                 int save = int.Parse(Console.ReadLine());
                 if (save == 1)
