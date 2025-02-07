cafbc79329f895bdb365cf52fd972df46043accb
Gaon99 <shk9680@icloud.com>
2025-02-07 15:08:37 +0900

    [Feat] buyScreen
    Add buyScreen Method

--- TeamTRPG_Project/Shop.cs
+++ TeamTRPG_Project/Shop.cs
@@ -27,7 +27,7 @@ namespace TeamTRPG_Project
             //초기에 설정한 아이템리스트들을 전부 표기
             for (int i = 0; i < item.itemList.Count; i++)
             {
-                Console.WriteLine($"- {itemList[i].ItemDisplay()} | {itemList[i].GetPriceString()}");
+                Console.WriteLine($"- {item.itemList[i].ItemDisplay()} | {item.itemList[i].GetPriceString()}");
             }
 
             Console.WriteLine();
@@ -42,11 +42,53 @@ namespace TeamTRPG_Project
                     gm.MainScreen();
                     break;
                 case 1:
-                    buyscreen();
+                    buyScreen();
                     break;
             }
         }
-        private void buyscreen()
+        private void buyScreen(bool needGold = false, bool hasItem = false)
+        {
+
+            Console.Clear();
+            ConsoleUtility.ColorWrite("상점 - 아이템 구매", ConsoleColor.Magenta);
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine();
+            Console.WriteLine("[보유 골드]");
+            Console.WriteLine($"{character.gold}G");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+
+            for (int i = 0; i < item.itemList.Count; i++)
+            {
+                Console.WriteLine($"- {i + 1}. {item.itemList[i].ItemDisplay()} | {item.itemList[i].GetPriceString()}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+
+            if (needGold)
+                Console.WriteLine("골드가 부족합니다!!");
+            else if (hasItem)
+                Console.WriteLine("이미 보유한 아이템입니다!!");
+
+            int input = ConsoleUtility.GetInput(0, item.itemList.Count);
+            switch (input)
+            {
+                case 0:
+                    gm.MainScreen();
+                    break;
+                default:
+                    Item select = item.itemList[input - 1];
+
+                    if (character.inventory.Contains(select))
+                        buyScreen(false, true); //아이템이 이미 보유중이라는 메세지 표기
+                    else
+                        buy(select); //아이템 구매 시도
+                    break;
+            }
+        }
+        private void buy(Item item)
         {
 
         }
